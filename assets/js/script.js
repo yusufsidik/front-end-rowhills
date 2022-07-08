05cd22d89313f5f5e4f10d6646286cdcc9d8dbff
yusufsidik <yusufsidik36@gmail.com>
2022-07-08 16:52:44 +0800

    add page list-category-product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+let categories = [];
 const fetchData = async function () {
   const url = 'http://127.0.0.1:8000/'
-  await fetch(`${url}api/products`, {
+  await fetch(`${url}api/categories`, {
       headers: {
         'Authorization': 'Bearer ' + '1|1F0F9UxOBu5dILilAuAvtgAl7kE0MOHFnqOjEu5W'
       }
@@ -8,8 +9,24 @@ const fetchData = async function () {
       return response.json();
     })
     .then(response => {
-      console.log(response.data.data)
+      categories.push(response.data.data)
+      console.log(categories[0])
     })
+}
+
+const fetchLoad = async (page) => {
+  const url = `http://127.0.0.1:8000/`
+  const token = `1|1F0F9UxOBu5dILilAuAvtgAl7kE0MOHFnqOjEu5W`
+  await fetch(`${url}api/categories?page=${page}`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  }).then(response => {
+    return response.json()
+  }).then(data => {
+    categories = [...categories[0], ...data.data.data]
+    console.log(categories)
+  })
 }
 
 fetchData();
